91c59fa94e9c92908d0664c2cfb8b0ea27b0329c
elnto de campos <eltoncampos36@gmail.com>
2021-01-20 14:48:13 -0300

    [Feat] - Create pagination

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,12 @@ import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import NextLink from 'next/link';
 import { Box, Button, Flex, Heading, Link, Stack, Text } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 
 const Index = () => {
+  const [variables, setVariables] = useState({ limit: 15, cursor: null as null | string})
   const [{ data, fetching }] = usePostsQuery({
-    variables: {
-      limit: 10,
-    }
-    
+    variables,
   });
 
     if (!fetching && !data) {
@@ -31,7 +29,7 @@ const Index = () => {
           <div>loading...</div>
         ): (
           <Stack spacing={8}>
-            { data!.posts.map((post) => (
+            { data!.posts.posts.map((post) => (
               <Box key={post.id} p={5} shadow="md" borderWidth="1px">
                 <Heading fontSize="xl">{post.title}</Heading>
                 <Text mt={4}>{post.textSnippet}</Text>
@@ -39,9 +37,14 @@ const Index = () => {
             ))}
           </Stack>
         )}
-          { data ?
+          { data && data.posts.hasMore ?
             (<Flex>
-              <Button isLoading={fetching} m="auto" my={8}>load more</Button>
+              <Button onClick={() => {
+                setVariables({
+                  limit: variables.limit,
+                  cursor: data.posts.posts[data.posts.posts.length -1].createdAt,
+                })
+              }} isLoading={fetching} m="auto" my={8}>load more</Button>
             </Flex>) 
           : null}
       </Layout>
